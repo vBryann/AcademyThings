b8603c3a62b7969399b8869a039bf480556cf0c2
vBryann <vitorbryan_melo@hotmail.com>
2020-03-27 11:43:03 -0300

    Atualizações de conteudos

--- LearningObjectives.playground/Contents.swift
+++ LearningObjectives.playground/Contents.swift
@@ -87,5 +87,90 @@ training{ condition in // forma 'reduzida' de representar uma closure recebendo 
 }
 
 /*
- 
+    Structs and Classes | Inheritance
  */
+
+struct Stop{
+    var plate: String
+}
+var point = Stop(plate: "Yameru")
+
+struct Thing{
+    var isAThing : Bool
+    var whatIs : String{
+        if isAThing {
+            return "this is a thing"
+        }else{
+            return "this isn't a thing"
+        }
+    }
+}
+var dale = Thing(isAThing: true)
+//print(dale.whatIs) //referenciando a variavel que contem a string
+
+struct Progress{
+    var task: String
+    var amount: Int{
+        didSet{ //existem duas formas de aplicar property, com willSet e didSet
+            print("O processo \(task) está em \(amount)%")
+        }
+    }
+}
+var prop = Progress(task:"comendo",amount: 0)
+prop.amount = 10
+prop.amount = 12
+
+//Funções dentro de structs são chamadas de métodos, mas tendo a mesma escrita de uma função
+struct Translate{
+    var traveledInMeters : Int
+    func traveledInKilometers() -> Int { //Você tbm pode utilizar metodos mutating, com a opção de mudar os argumentos da função ao instanciar-la
+        return traveledInMeters / 1000
+    }
+}
+let fortalezaMaranguape = Translate(traveledInMeters: 20000)
+fortalezaMaranguape.traveledInKilometers()
+
+//uso do init
+struct User{
+    var username: String
+    init(){
+        username = "Anonymous"
+        print("creating user")
+    }
+}
+var child = User()
+child.username = "Junin"
+
+
+//uso do self and lazy
+struct Group{
+    init(){
+        print("seu grupo foi criado")
+    }
+}
+struct Person {
+    var name: String
+    lazy var group = Group()//ao usar o lazy, o swift so vai criar a struct Group qnd for acessada pela primeira vez
+    init(nome: String) {
+        print("\(nome) was born!")
+        self.name = nome //self.name vai referenciar o name dado na struct (property), e nome vai referenciar o parametro
+    }
+}
+
+//------ Classes
+class Dog{
+    var name : String
+    var raca : String
+    init(name: String, raca: String){     //Uma das diferenças de classe pra struct, é que vc precisa criar inicializadores pra cada properties
+        self.name = name
+        self.raca = raca
+    }
+}
+class Husky: Dog{ //Para aplicar o sistema de subclasse (herança), é preciso referenciar a classe Pai,
+    //tendo por padrão as properties e inicializadores da classe herdada
+    init(name: String){
+        super.init(name: name, raca: "Husky Siberiano") //a chamada super.init na classe herdeira que permite utilizar o init da classe pai
+    }
+}
+
+
